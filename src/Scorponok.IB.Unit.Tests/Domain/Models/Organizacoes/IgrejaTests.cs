0c429cbd60dda8a821f9ea4f6edb7dc442eecd48
Artur Araújo Santos Ribeiro <arturrj@hotmail.com>
2018-05-05 13:21:54 -0300

    Incluindo mais um valueobject

--- src/Scorponok.IB.Unit.Tests/Domain/Models/Organizacoes/IgrejaTests.cs
+++ src/Scorponok.IB.Unit.Tests/Domain/Models/Organizacoes/IgrejaTests.cs
@@ -12,83 +12,83 @@ namespace Scorponok.IB.Unit.Tests.Domain.Models.Organizacoes
 		public void Deve_cria_entidade_igreja_so_com_nome()
 		{
 			//Arrange's
-			var igrega = Igreja.Factory.CreateNew("nome");
+			var igreja = Igreja.Factory.CreateNew("nome");
 
 			//Assert's
-			igrega.Should().NotBeNull();
-			igrega.Nome.Should().Be("nome");
+			igreja.Should().NotBeNull();
+			igreja.Nome.Should().Be("nome");
 		}
 
 		[Test]
 		public void Deve_cria_entidade_igreja_so_com_nome_foto()
 		{
 			//Arrange's
-			var igrega = Igreja.Factory.CreateNew("nome", "foto");
+			var igreja = Igreja.Factory.CreateNew("nome", "foto");
 
 			//Assert's
-			igrega.Should().NotBeNull();
-			igrega.Nome.Should().Be("nome");
-			igrega.Foto.Should().Be("foto");
+			igreja.Should().NotBeNull();
+			igreja.Nome.Should().Be("nome");
+			igreja.Foto.Should().Be("foto");
 		}
 
 		[Test]
 		public void Deve_cria_entidade_igreja_so_com_nome_foto_email()
 		{
 			//Arrange's
-			var igrega = Igreja.Factory.CreateNew("nome", "foto", Email.Factory.CreateNew("scorponok@scorponok.com"));
+			var igreja = Igreja.Factory.CreateNew("nome", "foto", Email.Factory.CreateNew("scorponok@scorponok.com"));
 
 			//Assert's
-			igrega.Should().NotBeNull();
-			igrega.Nome.Should().Be("nome");
-			igrega.Foto.Should().Be("foto");
-			igrega.Email.Should().NotBeNull();
-			igrega.Email.Value.Should().Be("scorponok@scorponok.com");
-			igrega.Email.Valido.Should().BeTrue();
-			igrega.Email.Mensagem.Should().BeNullOrEmpty();
+			igreja.Should().NotBeNull();
+			igreja.Nome.Should().Be("nome");
+			igreja.Foto.Should().Be("foto");
+			igreja.Email.Should().NotBeNull();
+			igreja.Email.Value.Should().Be("scorponok@scorponok.com");
+			igreja.Email.Valido.Should().BeTrue();
+			igreja.Email.Mensagem.Should().BeNullOrEmpty();
 		}
 
 		[Test]
 		public void Deve_cria_entidade_igreja_so_com_nome_foto_email_telefone()
 		{
 			//Arrange's
-			var igrega = Igreja.Factory.CreateNew("nome", "foto", Email.Factory.CreateNew("scorponok@scorponok.com"), Telefone.Factory.CreateNew(55, "21", "987413955"));
+			var igreja = Igreja.Factory.CreateNew("nome", "foto", Email.Factory.CreateNew("scorponok@scorponok.com"), Telefone.Factory.CreateNew(55, "21", "987413955"));
 
 			//Assert's
-			igrega.Should().NotBeNull();
-			igrega.Nome.Should().Be("nome");
-			igrega.Foto.Should().Be("foto");
+			igreja.Should().NotBeNull();
+			igreja.Nome.Should().Be("nome");
+			igreja.Foto.Should().Be("foto");
 
-			igrega.Email.Should().NotBeNull();
-			igrega.Email.Value.Should().Be("scorponok@scorponok.com");
-			igrega.Email.Valido.Should().BeTrue();
-			igrega.Email.Mensagem.Should().BeNullOrEmpty();
+			igreja.Email.Should().NotBeNull();
+			igreja.Email.Value.Should().Be("scorponok@scorponok.com");
+			igreja.Email.Valido.Should().BeTrue();
+			igreja.Email.Mensagem.Should().BeNullOrEmpty();
 
-			igrega.Telefone.Numero.Should().Be("5521987413955");
-			igrega.Telefone.Valido.Should().BeTrue();
-			igrega.Telefone.Mensagem.Should().BeNullOrEmpty();
-			igrega.Telefone.Mensagem.Should().BeNullOrEmpty();
+			igreja.Telefone.Numero.Should().Be("5521987413955");
+			igreja.Telefone.Valido.Should().BeTrue();
+			igreja.Telefone.Mensagem.Should().BeNullOrEmpty();
+			igreja.Telefone.Mensagem.Should().BeNullOrEmpty();
 		}
 
 		[Test]
 		public void Deve_cria_entidade_igreja_so_com_nome_foto_email_telefone_endereco()
 		{
 			//Arrange's
-			var igrega = Igreja.Factory.CreateNew("nome", "foto", Email.Factory.CreateNew("scorponok@scorponok.com"), Telefone.Factory.CreateNew(55, "21", "987413955"), Endereco.Factory.Empty());
+			var igreja = Igreja.Factory.CreateNew("nome", "foto", Email.Factory.CreateNew("scorponok@scorponok.com"), Telefone.Factory.CreateNew(55, "21", "987413955"), Endereco.Factory.Empty());
 
 			//Assert's
-			igrega.Should().NotBeNull();
-			igrega.Nome.Should().Be("nome");
-			igrega.Foto.Should().Be("foto");
+			igreja.Should().NotBeNull();
+			igreja.Nome.Should().Be("nome");
+			igreja.Foto.Should().Be("foto");
 
-			igrega.Email.Should().NotBeNull();
-			igrega.Email.Value.Should().Be("scorponok@scorponok.com");
-			igrega.Email.Valido.Should().BeTrue();
-			igrega.Email.Mensagem.Should().BeNullOrEmpty();
+			igreja.Email.Should().NotBeNull();
+			igreja.Email.Value.Should().Be("scorponok@scorponok.com");
+			igreja.Email.Valido.Should().BeTrue();
+			igreja.Email.Mensagem.Should().BeNullOrEmpty();
 
-			igrega.Telefone.Numero.Should().Be("5521987413955");
-			igrega.Telefone.Valido.Should().BeTrue();
-			igrega.Telefone.Mensagem.Should().BeNullOrEmpty();
-			igrega.Telefone.Mensagem.Should().BeNullOrEmpty();
+			igreja.Telefone.Numero.Should().Be("5521987413955");
+			igreja.Telefone.Valido.Should().BeTrue();
+			igreja.Telefone.Mensagem.Should().BeNullOrEmpty();
+			igreja.Telefone.Mensagem.Should().BeNullOrEmpty();
 		}
 	}
 }
