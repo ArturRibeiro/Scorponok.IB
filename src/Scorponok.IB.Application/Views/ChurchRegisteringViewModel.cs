c79d4234a605ff1c1d9827ae7007ed8dd0c1686c
Artur Araújo Santos Ribeiro <arturrj@hotmail.com>
2018-05-30 20:39:25 -0300

    include news files

---
+++ src/Scorponok.IB.Application/Views/ChurchRegisteringViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Scorponok.IB.Application.Views
+{
+    public class ChurchRegisteringViewModel
+    {
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Photo")]
+        public string Photo { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("MobileTelephone")]
+        public string MobileTelephone { get; set; }
+
+        [DisplayName("TelephoneFixed")]
+        public string TelephoneFixed { get; set; }
+    }
+}
---
+++ src/Scorponok.IB.Web.Api/Controllers/ChurchController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Scorponok.IB.Application.Views;
+
+namespace Scorponok.IB.Web.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Church")]
+    public class ChurchController : Controller
+    {
+        // GET: api/Church
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Church/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Church
+        [HttpPost]
+        public void Post([FromBody]ChurchRegisteringViewModel view)
+        {
+        }
+        
+        // PUT: api/Church/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
